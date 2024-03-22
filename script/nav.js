282fca5a914aa2f33c02cdb7aa40bf2f3e039aff
Kim Berntson <kimberntson@mac.com>
2024-03-22 14:10:14 +0100

    slideshow added

---
+++ script/nav.js
@@ -0,0 +1,38 @@
+// Nav component
+
+class Nav extends HTMLElement {
+
+  // The browser calls this method when the element is
+  // added to the DOM.
+
+  connectedCallback() {
+
+     this.innerHTML = `
+    <div id="nav">
+      <p>Tillbaka</p>
+     </div>
+    `
+  };
+};
+
+customElements.define('nav-component', Nav);
+
+// Footer component
+
+class Footer extends HTMLElement {
+  
+  // The browser calls this method when the element is
+  // added to the DOM.
+
+  connectedCallback() {
+ 
+     this.innerHTML = `
+    <div id="footer">
+      <p>Hello footer</p>
+      <p>Next line</p>
+    </div>
+    `
+  };
+};
+
+customElements.define('footer-component', Footer);
